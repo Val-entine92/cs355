7ac3a37fa3287ba6bd3b41702c3c5eda364c4364
Val-entine92 <pm5132003@gmail.com>
2023-02-02 13:52:15 -0500

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const $=document.querySelector.bind(document);
+
+
+function clickMe(){
+setInterval(//runs the function at at interval of 1 second
+    function getCurrentTime(){//function to obtain time ,day, date info and put into HTMl
+      let currentTimeDate = new Date();
+      var day= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+      var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+      let currentDay = day[currentTimeDate.getDay()];
+      let currentDate  = currentTimeDate.getDate();
+      let currentMonth = month[currentTimeDate.getMonth()];
+      let currentYear = currentTimeDate.getFullYear();
+      let fullDate = ` ${currentDay} ${currentDate} ${currentMonth}, ${currentYear}`;
+      $(".time").innerHTML =`[${fullDate} ] ` + currentTimeDate.toLocaleTimeString();
+  });
+
+}
+
+
+
