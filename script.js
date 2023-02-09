d666188d912a930ef3f6489a2e5c10e78f0b16a6
Val-entine92 <pm5132003@gmail.com>
2023-02-08 23:27:58 -0500

    updates to html/js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 const $=document.querySelector.bind(document);
 const time=document.getElementsByClassName(".time");
+const backgroundImage=document.getElementsByClassName(".bg-Img")
 
 
 
 function clickMe(){
-setInterval(//runs the function at at interval of 1 second
-    function getCurrentTime(){//function to obtain time ,day, date info and put into HTMl
+  setInterval(
+  function getCurrentTime(){//function to obtain time ,day, date info and put into HTMl
       let currentTimeDate = new Date();
       var day= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
       var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
@@ -17,9 +18,14 @@ setInterval(//runs the function at at interval of 1 second
       let currentMonth = month[currentTimeDate.getMonth()];
       let currentYear = currentTimeDate.getFullYear();
       let fullDate = ` ${currentDay} ${currentDate} ${currentMonth}, ${currentYear}`;
+      $(".time").classList.add("active");
       $(".time").innerHTML =`[${fullDate} ] ` + currentTimeDate.toLocaleTimeString();
   });
 
+}
+
+function changeColor(){
+  document.body.style.background="cadetblue";
 }
 
 
